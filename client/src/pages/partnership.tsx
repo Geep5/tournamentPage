604528442ce1ca00832d59d1df616352d2b2f42e
Grant Farwell <grantgfarwell@gmail.com>
2026-03-06 17:16:51 -0800

    fix: missing map closure in partnership JSX

--- client/src/pages/partnership.tsx
+++ client/src/pages/partnership.tsx
@@ -298,6 +298,7 @@ export default function PartnershipPage() {
                         </div>
                       </div>
                     </button>
+                  ))}
                 </div>
               </>
             ) : (
